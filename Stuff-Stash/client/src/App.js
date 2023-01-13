30f31dd2485196080f4507224742c03c22fee153
halsowadi <halsowadi1@gmail.com>
2023-01-13 12:52:02 -0500

    *Cleaned up project a bit. ~Cleaned up REST API, removed duplicated functions & formatted ~Removed old and nonfunctioning pages from frontend.

--- Stuff-Stash/client/src/App.js
+++ Stuff-Stash/client/src/App.js
@@ -1,12 +1,9 @@
 import {
-	BrowserRouter as Redirect,
-	Route,
-	Switch,
-	useHistory,
+  BrowserRouter as Redirect,
+  Route,
+  Switch,
+  useHistory,
 } from "react-router-dom";
-
-import Helpme from "./Pages/ListOfUsers";
-import UserLogin from "./Pages/login";
 import React from "react";
 import RegForm from "./components/registrationform";
 import welcome from "./components/welcome2";
@@ -16,8 +13,6 @@ import CreateOrg from "./components/CreateOrg";
 import CreateStockroom from "./Pages/CreateStockRoom";
 import RenameOrgPage from "./components/RenameOrg";
 import AddUserOrg from "./Pages/AddUserOrg";
-import viewstockroomFrontend from "./Pages/viewstockroomFrontend";
-import viewOrgMembers from "./Pages/viewOrgMembers";
 import AssetForm from "./Pages/addAssetForm";
 import assetCard from "./components/assetCard";
 import "./App.css";
@@ -28,24 +23,24 @@ ReactSession.setStoreType("localStorage");
 const user = ReactSession.get("username");
 
 function App() {
-	let pathname = window.location.pathname;
-	let history = useHistory();
+  let pathname = window.location.pathname;
+  let history = useHistory();
 
-	function checkRefresh() {
-		console.log(pathname);
-		history.push("/");
-		window.location.reload();
-	}
+  function checkRefresh() {
+    console.log(pathname);
+    history.push("/");
+    window.location.reload();
+  }
 
-	// when this constant is called, it checks if there is a username stored in the session variables
-	const isLoggedIn = () => {
-		if (user != null) {
-			console.log(user);
-			return true;
-		} else {
-			return false;
-		}
-	};
+  // when this constant is called, it checks if there is a username stored in the session variables
+  const isLoggedIn = () => {
+    if (user != null) {
+      console.log(user);
+      return true;
+    } else {
+      return false;
+    }
+  };
   return (
     <div className="App">
       {!isLoggedIn() && pathname !== "/" && pathname !== "/reg"
@@ -53,17 +48,6 @@ function App() {
         : console.log("Rerouting")}
 
       <Switch>
-        <Route path="/list" exact component={Helpme} />
-        <Route
-          path="/viewstockroomFrontend"
-          exact
-          component={viewstockroomFrontend}
-        />
-        <Route
-						path="/viewOrgMember"
-						exact
-						component={viewOrgMembers}
-					/>
         <Route path="/reg" exact component={RegForm} />
         <Route path="/dashboard" exact component={Dashboard} />
         <Route path="/Org" exact component={CreateOrg} />
@@ -71,9 +55,7 @@ function App() {
         <Route path="/createStockRoom" exact component={CreateStockroom} />
         <Route path="/adduserOrg" exact component={AddUserOrg} />
         <Route path="/addAssetForm" exact component={AssetForm} />
-
         <Route path="/asset" exact component={assetCard} />
-
         <Route path="/" exact component={welcome} />
         <Redirect from="*" to="/" />
       </Switch>
--- Stuff-Stash/client/src/utils/UpdateForm.jsx
+++ Stuff-Stash/client/src/utils/UpdateForm.jsx
@@ -2,93 +2,89 @@ import React from "react";
 import { ReactSession } from "react-client-session";
 import "../components/styles/assetCard.css";
 
-var name ="";
-var serialCode ="";
-var availability ="";
-var category ="";
-var condition ="";
-var warranty ="";
-
-
-
+var name = "";
+var serialCode = "";
+var availability = "";
+var category = "";
+var condition = "";
+var warranty = "";
 
 const UpdateForm = () => {
-
-  const setNewName= (e) => {
+  const setNewName = (e) => {
     name = e.target.value;
   };
-  const setNewSerialCode= (e) => {
+  const setNewSerialCode = (e) => {
     serialCode = e.target.value;
   };
-  const setNewAvailabity= (e) => {
+  const setNewAvailabity = (e) => {
     availability = e.target.value;
   };
-  const setCategory= (e) => {
+  const setCategory = (e) => {
     category = e.target.value;
   };
-  const setCondition= (e) => {
+  const setCondition = (e) => {
     condition = e.target.value;
   };
-  const setWarranty= (e) => {
+  const setWarranty = (e) => {
     warranty = e.target.value;
   };
 
-  const update =  async (event) => {
+  const update = async (event) => {
     // console.log("your identifer is" + oldIdentifer+"your old stockroom is " + oldIdentifer +"and your newname is"+newName);
     event.preventDefault();
     try {
-     const response =  await fetch("http://localhost:3000/api/v1/UpdateAsset", {
-       method: "POST",
-       body: JSON.stringify({
-         stockroomName:ReactSession.get("selectedStockroom"),
-         newIdentifer:name,  
-         identifier:ReactSession.get("identiferName"),
-         newSerialCode:serialCode,
-         newIsAvailable:availability,
-         newCategory:category,
-         newCondition:condition,
-         newWarranty:warranty
-       }),
-       headers: {
-         "Content-Type": "application/json",
-         Accept: "application/json",
-       },
-     });
-  
-     if (!response.ok) {
-       throw new Error(`Error! status: ${response.status}`);
-     }
-     
-     const result =  await response.json();
-  
-     console.log("result is: ", JSON.stringify(result, null, 4));
-   } catch (err) {
-   } finally {
-     window.location.reload();
-   }
-   };
+      const response = await fetch("http://localhost:3000/api/v1/UpdateAsset", {
+        method: "POST",
+        body: JSON.stringify({
+          stockroomName: ReactSession.get("selectedStockroom"),
+          newIdentifer: name,
+          identifier: ReactSession.get("identiferName"),
+          newSerialCode: serialCode,
+          newIsAvailable: availability,
+          newCategory: category,
+          newCondition: condition,
+          newWarranty: warranty,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log("result is: ", JSON.stringify(result, null, 4));
+    } catch (err) {
+    } finally {
+      window.location.reload();
+    }
+  };
   return (
     <form onSubmit="{update}">
       <div class="form-group m-3 black">
-        <label for="Name" >Name : </label>
-        <input onChange={setNewName}/>
+        <label for="Name">Name : </label>
+        <input onChange={setNewName} />
       </div>
       <div class="form-group m-3 black">
         <label for="Serial Code">Serial Code : </label>
-        <input onChange={setNewSerialCode}/>
+        <input onChange={setNewSerialCode} />
       </div>
       <div class="form-group m-3 black">
         <label for="avail">Availability :</label>
-        <input onChange={setNewAvailabity}/>
+        <input onChange={setNewAvailabity} />
       </div>
 
       <div class="form-group m-3 black">
         <label class="form-label" for="asset info">
-          Asset Info :
+          Category :
         </label>
-        <input onChange={setCategory}/>
+        <input onChange={setCategory} />
       </div>
-      <div class="form-group m-3 black" >
+      <div class="form-group m-3 black">
         <label for="condition">Condition :</label>
         <select name="condition" id="condition" onChange={setCondition}>
           <option>Select Value</option>
@@ -99,13 +95,13 @@ const UpdateForm = () => {
       </div>
       <div class="form-group m-3 black">
         <label for="WarrDate">Warranty Date :</label>
-        <input onChange={setWarranty}/>
-       <div className="text-center">
-        <button className="btn btn-primary m-3 " onClick={update}>
-          Confirm{" "}
-        </button>
-      </div></div>
-     
+        <input onChange={setWarranty} />
+        <div className="text-center">
+          <button className="btn btn-primary m-3 " onClick={update}>
+            Confirm{" "}
+          </button>
+        </div>
+      </div>
     </form>
   );
 };
